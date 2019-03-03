97cf32ad9b2a178a0a8133c2b517dc8cf531bb82
jogunbona <ogunbona.gbolahan@gmail.com>
2019-03-03 12:34:48 -0800

    Renamed UnitTest1 class to WorkdayTests

---
+++ Interview.Test/WorkdayTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Interview.Test
+{
+	[TestClass]
+	public class WorkdayTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
